e3451f4a5431776783639341abf964e6d4dbf1c4
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2021-10-10 14:43:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function preload(){
+
+}
+
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+
+function take_snapshot(){
+    save('Melaina_114_class.png');
+}
